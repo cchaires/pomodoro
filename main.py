d557d1af025bcd2fa8b89d5cd780b5328b5527dd
Carlos Chaires <chairescarlos@gmail.com>
2023-04-03 20:35:15 -0700

    Fixing some bugs, now the start button only runs once until the timer is stopped or press the reset button, and now the check appears with work.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 BACKGROUND = "#454545"
@@ -7,43 +8,52 @@ C3 = "#FFA559"
 C4 = "#FFE6C7"
 FONT_NAME = "Fira Code"
 WORK_MIN = 25
-SHORT_BREAK_MIN = 1
+SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+SEC = 60
 REPS = 1
 check = ""
-timer = None
+timer = ""
+timer_is_running = False
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    global REPS, timer, check
+    global REPS, timer, check, timer_is_running
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
     timer_label.config(text="Timer")
     REPS = 1
     check = ""
+    check_text.config(text=check)
+    timer_is_running = False
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    global REPS
+    global REPS, timer_is_running, check
 
-    if REPS % 2 == 0:
-        count_down(SHORT_BREAK_MIN * 60)
-        timer_label.config(text="Short break", fg=C2)
-    elif REPS == 8:
-        count_down(LONG_BREAK_MIN * 60)
-        timer_label.config(text="Long break", fg=C3)
-    else:
-        count_down(WORK_MIN * 60)
-        timer_label.config(text="Work", fg=C4)
-    REPS += 1
+    if not timer_is_running:
+        timer_is_running = True
+        if REPS % 2 == 0:
+            count_down(SHORT_BREAK_MIN * SEC)
+            timer_label.config(text="Short break", fg=C2)
+        elif REPS == 8:
+            count_down(LONG_BREAK_MIN * SEC)
+            timer_label.config(text="Long break", fg=C3)
+            timer_is_running = False
+        else:
+            count_down(WORK_MIN * SEC)
+            timer_label.config(text="Work", fg=C4)
+            check += "✔"
+            check_text.config(text=check)
+        REPS += 1
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    global check, REPS, timer
-    count_min = int(count / 60)
+    global check, REPS, timer, timer_is_running
+    count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
         count_sec = f"0{count_sec}"
@@ -53,11 +63,8 @@ def count_down(count):
     if count > 0:
         timer = window.after(1000, count_down, count - 1)
     else:
+        timer_is_running = False
         start_timer()
-        works_session = int(REPS / 2)
-        for _ in range(works_session):
-            check += "✔"
-        check_text.config(text=check)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
